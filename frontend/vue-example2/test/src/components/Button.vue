7315b2bfab3f3c090b5ddb7f18c62d05875412bd
danzziy <dannyazzawi@gmail.com>
2023-12-28 23:33:05 -0500

    Style the button component

--- frontend/vue-example2/test/src/components/Button.vue
+++ frontend/vue-example2/test/src/components/Button.vue
@@ -1,5 +1,5 @@
 <template>
-    <button @click="onClick()" :style="{background: color}">{{ text }}</button>
+    <div :style="{background: color}">{{ text }}</div>
 </template>
 
 <script>
@@ -9,11 +9,27 @@ export default {
         text: String,
         color: String,
     },
-    methods: {
-        onClick() {
-            console.log('click')
-        }
-    }
 }
 </script>
 
+<style scoped>
+div {
+    width: 520px;
+    height: 40px;
+    display: inline-flex;
+    translate: 500px;
+
+    font-family: '8bit';
+    font-size: 36px;
+    justify-content: center;
+    color: white;
+    
+    cursor: pointer;
+
+    transition: font-size 0.6s ease, font-weight 1s ease; 
+}
+div:hover {
+    font-size: 52px; /* Larger font size when hovered */
+
+}
+</style>
